d8830eee813e69e30666d98ea707e5468cb3fc4f
Andy Karpov <andy.karpov@gmail.com>
2017-09-21 10:24:03 +0300

    Added a second state with color test to the example sketch

--- ino/SPIVGA/examples/sd_list/sd_list.ino
+++ ino/SPIVGA/examples/sd_list/sd_list.ino
@@ -10,11 +10,20 @@
 #define SPI_SD_CS  9
 #define SPI_FLASH_CS  8
 #define SERIAL_SPEED 9600
+#define MAX_STATES 2
 
 SPIVGA vga(SPI_VGA_CS);
 KeyboardReport report;
 File root;
 uint8_t tick = 0;
+bool sd_init = false;
+
+enum app_states_t {
+  STATE_MAIN,
+  STATE_COLORS
+};
+
+uint8_t state = STATE_MAIN;
 
 void setup()
 {
@@ -29,10 +38,17 @@ void setup()
 
     SPI.begin();
     vga.begin();
+
     vga.setColor(vga.COLOR_WHITE);
     vga.setBackground(vga.COLOR_BLACK);
     vga.clear();
+    sd_init = SD.begin(SPI_SD_CS);
 
+    appMain();
+}
+
+void appMain() {
+    vga.setColor(vga.COLOR_WHITE);
     vga.setBackground(vga.COLOR_BLUE);
     vga.fill(10, 5, 69, 24, 0);
     vga.frame(10, 5, 69, 24, 1);
@@ -46,17 +62,47 @@ void setup()
     vga.setPos(20, 10);
     vga.print(F("Init SD Card... "));
 
-    if (!SD.begin(SPI_SD_CS)) {
+    if (sd_init) {
+      vga.setColor(vga.COLOR_GREEN_I);
+      vga.print(F("DONE"));
+      vga.setColor(vga.COLOR_WHITE);
+  
+      root = SD.open("/");
+      printDirectory(root);
+      root.close();
+    } else {
       vga.setColor(vga.COLOR_RED_I);
-      vga.print(F("FAILED"));
-      return;
+      vga.print(F("DONE"));
+      vga.setColor(vga.COLOR_WHITE);      
     }
-    vga.setColor(vga.COLOR_GREEN_I);
-    vga.print(F("DONE"));
+    
+    vga.setPos(21, 22);
     vga.setColor(vga.COLOR_WHITE);
+    vga.setBackground(vga.COLOR_RED_I);
+    vga.print(F("      Press ENTER to continue...      "));  
+}
 
-    root = SD.open("/");
-    printDirectory(root);
+void appColors() {
+    vga.setColor(vga.COLOR_WHITE);
+    vga.setBackground(vga.COLOR_BLUE);
+    vga.fill(10, 5, 69, 24, 0);
+    vga.frame(10, 5, 69, 24, 1);
+    vga.setPos(65, 5);
+    vga.print(F("["));
+    vga.write(254);
+    vga.print(F("]"));
+    vga.setPos(12,5);
+    vga.print(F("[Test colors]"));
+
+    for (uint8_t i=0; i<14; i++) {
+      vga.setPos(16, i+7);
+      for (uint8_t c=0; c<16; c++) {
+        vga.setColor(c);
+        vga.write(219);
+        vga.write(219);
+        vga.write(219);
+      }
+    }
 
     vga.setPos(21, 22);
     vga.setColor(vga.COLOR_WHITE);
@@ -92,12 +138,28 @@ void printDirectory(File dir) {
 
 void loop()
 {
-  vga.setPos(75,29);
+  vga.setPos(68,29);
   report = vga.getReport();
   vga.setColor(vga.COLOR_CYAN_I);
-  vga.setBackground(vga.COLOR_MAGENTA);
+  vga.setBackground(vga.COLOR_BLACK);
+  vga.print(F("DEBUG:"));
+  vga.setColor(vga.COLOR_WHITE);
   vga.print(report.key1);
   vga.print(F("  "));
   tick++;
-  delay(100);
+  if (report.key1 == 40) {
+    state++;
+    if (state >= MAX_STATES) {
+      state = 0;
+    }
+    switch(state) {
+      case STATE_MAIN:
+        appMain();
+        break;
+      case STATE_COLORS:  
+        appColors();
+        break;
+    }
+  }
+  delay(10);
 }
